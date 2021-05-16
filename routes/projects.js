1ff1f22f1bab9a16d2d198dfe532a4323bfeaeee
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-05-16 01:08:08 -0400

    Fix images and css url root doesn't start with '/'

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const yaml = require('js-yaml');
+
 /* GET users listing. */
 router.get('/:name', function (req, res, next) {
     const name = req.params.name;
+    const data = yaml.load(fs.readFileSync(appRoot + '/views/pages/projects/' + name + '.yml'));
 
-    res.send('respond with a resource for ' + name);
+    res.render('pages/project', data);
 });
 
 module.exports = router;

3abe6abea4ddd6eb6da5a7e8793c510f55010b4b
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-05-15 23:09:31 -0400

    Finish ejs skills

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    const data = require(appRoot+'/views/pages/index.json');
+    const data = yaml.load(fs.readFileSync(appRoot+'/views/pages/index.yml'));
 
     res.render('pages/index', data);
 });

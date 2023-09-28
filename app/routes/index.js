df1d95d13124362059f6b9aa7703e8c5e8aed6ae
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-09-28 03:26:39 -0400

    Update to use more CDN

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const fs = require('fs');
-const yaml = require('js-yaml');
+const fs = require("fs");
+const yaml = require("js-yaml");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    const data = yaml.load(fs.readFileSync(appRoot+'/views/pages/index.yml'));
+router.get("/", function (req, res, next) {
+  const data = yaml.load(fs.readFileSync(appRoot + "/views/pages/index.yml"));
 
-    res.render('pages/index', data);
+  res.render("pages/index", data);
 });
 
 module.exports = router;
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const fs = require('fs');
-const yaml = require('js-yaml');
+const fs = require("fs");
+const yaml = require("js-yaml");
 
 /* GET users listing. */
-router.get('/:name', function (req, res, next) {
-    const name = req.params.name;
-    const data = yaml.load(fs.readFileSync(appRoot + '/views/pages/projects/' + name + '.yml'));
+router.get("/:name", function (req, res, next) {
+  const name = req.params.name;
+  const data = yaml.load(fs.readFileSync(appRoot + "/views/pages/projects/" + name + ".yml"));
 
-    res.render('pages/project', data);
+  res.render("pages/project", data);
 });
 
 module.exports = router;
